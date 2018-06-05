2965a409a3a262f80ebb53dbd1d6e36fc6986b42
FedorovRoman <studentutu@gmail.com>
2018-06-05 19:05:03 +0300

    added Main() (for test purposes)

--- Biird_Client/BiirdClient.cs
+++ Biird_Client/BiirdClient.cs
@@ -7,7 +7,34 @@ namespace Biird_Client
 {
     public class BiirdClient
     {
+        private static readonly string URL = "https://api.biird.io/";
+        private static readonly string resourceValueURL  = "https://api.biird.io/resourceValue/0";
 
+        public static void Main(strings[] args){
+            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(resourceValueURL);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body. Blocking!
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }  
+        }
     }
 
     class Entity
