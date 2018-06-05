e39143d3fbf1635b5c09b18d2a48945a989eb84e
RomanFedorov <studentutu@gmail.com>
2018-06-05 20:00:05 +0300

    test script for dotnet (for test purposes)

---
+++ Biird_Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+namespace Biird_Client
+{
+    class Program
+    {
+        public static void Main(string[] args){
+            
+            using (var wb = new WebClient())
+            {
+                var response = wb.DownloadString(BiirdClient.resourceValueURL);
+                Console.WriteLine(response);
+            }
+        }
+    }
+}
