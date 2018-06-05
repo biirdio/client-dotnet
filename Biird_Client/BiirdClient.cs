ef3714c8676dcde8bdb42186b20ab5c3370f8940
Stanislav <staskalyuk.096@gmail.com>
2018-06-05 18:42:21 +0300

    Biird class and entity class (strings only)

--- Biird_Client/BiirdClient.cs
+++ Biird_Client/BiirdClient.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Globalization;
 namespace Biird_Client
 {
     public class BiirdClient
@@ -12,14 +12,37 @@ namespace Biird_Client
 
     class Entity
     {
-
-    }
-    public static class Extentions
-    {
-        public static void Merge<TKey,TValue>(this IDictionary<TKey,TValue> dictionary)
+        public string Data;
+        public Entity(string data)
         {
-            var mutableCopy = new Dictionary<TKey,TValue>(dictionary);
-
+            Data = data;
         }
+        //todo check encoding and transfer to utf8
+    }
+
+    class Biird
+    {
+        public static Biird Shared = new Biird();
+        private string _languageCode;
+        private string _countryCode;
+        public Biird()
+        {
+            string[] code = CultureInfo.CurrentCulture.Name.Split('-');
+            if(code.Length == 2)
+            {
+                _languageCode = code[0];
+                _countryCode = code[1];
+            }
+            else
+            {
+                _languageCode = code[0];
+            }
+        }
+
+        public string DefaultLanguageDimentions()
+        {
+            return _languageCode;
+        }
+
     }
 }
