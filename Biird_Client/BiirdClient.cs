b071f1412497c0e09a69a599f8f54b9df4285a58
Stanislav <staskalyuk.096@gmail.com>
2018-06-07 17:52:35 +0300

    Finish current version

--- Biird_Client/BiirdClient.cs
+++ Biird_Client/BiirdClient.cs
@@ -1,98 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Net;
 
 namespace Biird_Client
 {
-    public class BiirdClient
+    public static class Biird
     {
-        public static readonly string URL = "https://api.biird.io/";
-        public static readonly string resourceValueURL  = "https://api.biird.io/resourceValue/";
+        private const string Url = "https://api.biird.io/resourceValue/";
+        private static string _currLangCode;
     
-    }
-
-    class Entity
-    {
-        public string Data;
-        public Entity(string data)
-        {
-            Data = data;
-        }
-        //todo check encoding and transfer to utf8
-    }
-
-    class Biird
-    {
-        public static Biird Shared = new Biird();
-        private string _languageCode;
-        private string _countryCode;
-
         /// <summary>
-        ///  Dictionary of Dimensions.
+        /// Initialize plugin
         /// </summary>
-        /// <param name="key"> Language Code. </param>
-        /// <param name="value"> Attribute, example: language </param>
-        private Dictionary<string, string> biirdBaseDimensions = new Dictionary<string, string>();
-        public Biird()
+        public static void Init()
         {
-            string[] code = CultureInfo.CurrentCulture.Name.Split('-');
-            // TO DO :
-            //       - call the default dimensions
-            Shared.DefaultLanguageDimentions();
-
-            if(code.Length == 2)
-            {
-                _languageCode = code[0];
-                _countryCode = code[1];
-            }
-            else
-            {
-                _languageCode = code[0];
-            }
-            
+            var code = CultureInfo.CurrentCulture.Name.Split('-');
+            _currLangCode = code[0]; //get current code
         }
 
-        public string DefaultLanguageDimentions()
-        {
-            biirdBaseDimensions.Add("de","language"); // for test purposes
-            return _languageCode;
-        }
-        
         /// <summary>
         ///  Call fetch to connect to the API
         /// </summary>
         /// <param name="id"> Id for the current item.</param>
-        public static string fetch(string id){
-            Entity currentEntity;
-            
+        public static string Fetch(string id)
+        {
             using (var wb = new WebClient())
             {
-                
-                string parameters = "?";
-                int i = 0;
+                var parameters = "?" + _currLangCode;
 
-                // add all attributes and its values to the parameters
-                foreach(string key in Shared.biirdBaseDimensions.Keys){
-                    parameters += Shared.biirdBaseDimensions[key] + "=" + key;
-                    if( i < Shared.biirdBaseDimensions.Keys.Count-1){
-                        parameters += "&";
-                    }
-                    i++;
-                }
-
-                // Console.WriteLine(BiirdClient.resourceValueURL + id + parameters);
                 // string finalString = "https://api.biird.io/resourceValue/b9fb0f44-31d5-45df-9ec3-776568802c31?language=en";
-                var response = wb.DownloadString(BiirdClient.resourceValueURL + id + parameters);
-                currentEntity = new Entity(response);
-                Console.WriteLine(currentEntity.Data);
+                var response = wb.DownloadString(Url + id + parameters);
                 return response;
             }
-          
-           
-
         }
 
     }
